1f0764aa7a246cf376261984f54fee01ea59d9d9
DMTTTinccc <dimatokwork@gmail.com>
2025-02-14 23:40:15 +0500

    My first commit

---
+++ dzz.py
@@ -0,0 +1,25 @@
+#Task 1
+massive=[9,5,1,4,7,9,3,5,7]
+num=sum(massive)
+print(num)
+
+#Task 2
+secondMassive=[1,2,3,4,5,6,7,8,9]
+for secmass in secondMassive:
+    if secmass%2==0:
+        print(secmass)
+
+#Task 3
+thirdMassive=[1,3,4,1995,6,7,8,92]
+dmt=max(thirdMassive)
+print(dmt)
+
+#Task 4
+fourthMassive=[3,7,36,9,4,8,43,65,8]
+for frthmss in range(8,-1,-1):
+    print(fourthMassive[frthmss])
+
+#Task 5
+for i in range(1,11):
+    result=i*5
+    print(result)
